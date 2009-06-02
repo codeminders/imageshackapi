f30a03aaf21d23825df114eeb7a3b7b95a333b06
Alexander Saveliev <lyolik1977@gmail.com>
2009-06-03 02:36:47 +0300

    PHP API, object-oriented style

---
+++ src/samples/python/yfrog_upload.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import yfrog
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv)<4:
+        print "Usage upload.py <username> <password> <filename/url> [<text>]"
+        sys.exit(1)
+    u = yfrog.YfrogUploader()
+    if len(sys.argv)==5:
+        msg = sys.argv[4]
+    else:
+        msg = None
+
+    try:
+        if sys.argv[3].startswith("http://"):
+            print u.uploadURL(sys.argv[3],sys.argv[1],sys.argv[2],message=msg)
+        else:
+            print u.uploadFile(sys.argv[3],sys.argv[1],sys.argv[2],message=msg)
+    except yfrog.YfrogServerException, e:
+        print str(e)
